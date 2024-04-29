6441b223f119b2486aec3f4cc3eb3f3281d317fc
Felipe Germano <fgermano@mail.austral.edu.ar>
2024-04-28 21:51:04 -0300

    changes in board

--- engine/src/main/kotlin/edu/austral/dissis/chess/engine/Game/Board.kt
+++ engine/src/main/kotlin/edu/austral/dissis/chess/engine/Game/Board.kt
@@ -3,29 +3,14 @@ package edu.austral.dissis.chess.engine.Game
 import edu.austral.dissis.chess.engine.Piece.Piece
 import edu.austral.dissis.chess.engine.Piece.Position
 
-class Board (val size: Position){
-  val board: Map<Position, Piece?> = mapOf()
+class Board (val size: Position, val pieces: Map<Position, Piece?>){
 
-  fun getPiece(position: Position): Piece? {
-    return board[position]
+  fun addPiece(position: Position, piece: Piece) {
+    // TODO
   }
 
-  fun createBoard(): Map<Position, Piece?> {
-    return (0 until size.getRow()).flatMap { i ->
-      (0 until size.getColumn()).map { j ->
-        Position(Pair(i, j)) to null
-      }
-    }.toMap()
-  }
-
-  fun addPiece(position: Position, piece: Piece): Board {
-    val newBoard = board.plus(position to piece)
-    return Board(size).apply { newBoard }
-  }
-
-  fun removePiece(position: Position): Board {
-    val newBoard = board.minus(position)
-    return Board(size).apply { newBoard }
+  fun removePiece(position: Position){
+    // TODO
   }
 
 }
